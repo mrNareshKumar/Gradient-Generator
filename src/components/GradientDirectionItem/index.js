298d7b09e11fe4d796dab05ac01524531fba3d0c
root <naresh.sakinala@outlook.com>
2023-01-28 04:04:42 +0000

    app builded using styled-components package concept

---
+++ src/components/GradientDirectionItem/index.js
@@ -0,0 +1,24 @@
+// Write your code here
+import {ListItem, DirectionButton} from './styledComponents'
+
+const GradientDirectionItem = props => {
+  const {gradientDirectionDetails, isActive, clickGradientDirectionItem} = props
+  const {displayText, value} = gradientDirectionDetails
+  const onClickGradientDirectionItem = () => {
+    clickGradientDirectionItem(value)
+  }
+
+  return (
+    <ListItem>
+      <DirectionButton
+        isActive={isActive}
+        type="button"
+        onClick={onClickGradientDirectionItem}
+      >
+        {displayText}
+      </DirectionButton>
+    </ListItem>
+  )
+}
+
+export default GradientDirectionItem
